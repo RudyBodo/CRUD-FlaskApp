2827e93b5f1735222689536c2af32c6545a4b2b0
rudybodo <transkumba@gmail.com>
2016-02-05 04:24:32 +0800

    fix add row

--- app/crud/models.py
+++ app/crud/models.py
@@ -4,14 +4,14 @@ class Barang(db.Model):
     __tablename__ = 'barang'
 
     id = db.Column(db.Integer, primary_key=True)
-    nama_barang = db.Column(db.String())
-    jenis_barang = db.Column(db.String())
-    jumlah = db.Column(db.String())
+    nama_barang = db.Column(db.String(50))
+    jenis_barang = db.Column(db.String(50))
+    jumlah = db.Column(db.Integer)
 
-def __init__(self, nama_barang, jenis_barang, jumlah):
-    self.nama_barang = nama_barang
-    self.jenis_barang = jenis_barang
-    self.jumlah = jumlah
+    def __init__(self, nama_barang, jenis_barang, jumlah):
+        self.nama_barang = nama_barang
+        self.jenis_barang = jenis_barang
+        self.jumlah = jumlah
 
-def __repr__(self):
-    return '<Barang{}>'.format(self.nama_barang)
+    def __repr__(self):
+        return '<Barang {}>'.format(self.nama_barang)
