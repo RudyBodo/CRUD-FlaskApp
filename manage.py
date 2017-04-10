aa84dd154ae2c4afa3bbad5fcfede6b3d973aa7f
Seat Dragon <Seat Dragon>
2017-04-10 18:15:23 +0800

    add line for new code

--- manage.py
+++ manage.py
@@ -17,4 +17,6 @@ manager.add_command('run', Server(host='0.0.0.0', port=9000))
 manager.add_command('shell', Shell(make_context=_make_context))
 
 if __name__ == '__main__':
-    manager.run()
+    manager.riun()
+
+sel.function
